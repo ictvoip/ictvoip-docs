987bb1ca07234e53d5e7cadb3d321edde5629615
bayden10 <jevans@icttech.net>
2022-10-22 14:36:04 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,10 @@
 
 # -- Project information
 
-project = 'ictVoIP Billing'
-copyright = '2022, ictVoIP Canada'
-author = 'Jim'
+project = u'ictVoIP Billing'
+copyright = u'2022, ictVoIP Canada'
 
-release = '0.1'
-version = '0.1.0'
+version = '1.3'
 
 # -- General configuration
 
@@ -30,6 +28,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_favicon = "_static/images/favicon.ico"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
